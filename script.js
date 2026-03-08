7948c6301ce93da0b7b0dbfe7f30f97058a6dcfd
davc25 <150166551+davc25@users.noreply.github.com>
2026-03-08 21:46:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,22 +11,23 @@ function createPetal(angle, distance, delay){
 
     heart.style.setProperty("--x", x);
     heart.style.setProperty("--y", y);
+
     heart.style.animationDelay = delay + "s";
 
     flower.appendChild(heart);
 }
 
-function bloomLily(){
+function bloom(){
 
     const petals = 8;
-    const distance = 40;
+    const radius = 50;
 
     for(let i=0;i<petals;i++){
 
-        const angle = (i / petals) * Math.PI * 2;
+        const angle = (i/petals) * Math.PI * 2;
 
-        createPetal(angle, distance, i*0.2);
+        createPetal(angle, radius, i*0.15);
     }
 }
 
-setTimeout(bloomLily,3000); 
+setTimeout(bloom,3000);
